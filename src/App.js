029d5b5a07debf9a94c73f65de12e1ed62f7fed0
ChrisferDev <ferchris82@hotmail.com>
2024-03-18 14:35:50 -0500

    Versión 1.3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ListadoEmpleados from "./empleados/ListadoEmpleados";
+import Navegacion from "./plantilla/Navegacion";
 
 function App() {
   return (
-    <ListadoEmpleados/>
+    <div className="container">
+      <BrowserRouter>
+        <Navegacion />
+        <Routes>
+          <Route exact path='/' element={<ListadoEmpleados />} />
+        </Routes>
+      </BrowserRouter>
+
+    </div>
   );
 }
 
---
+++ src/plantilla/Navegacion.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Navegacion() {
+  return (
+    <div className='container'>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">Sistema de Recursos Humanos</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" 
+          href="/">Inicio</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/agregar">Agregar Empleado</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    </div>
+  )
+}
