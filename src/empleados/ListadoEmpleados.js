0a16334e859b6353323e8da1a640b52e65b1db88
ChrisferDev <ferchris82@hotmail.com>
2024-03-18 14:00:16 -0500

    Versión 1.2

--- src/empleados/ListadoEmpleados.js
+++ src/empleados/ListadoEmpleados.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import { FormattedNumber, IntlProvider } from 'react-intl';
 
 export default function ListadoEmpleados() {
 
     const urlBase = "http://localhost:8080/rh-app/empleados";
 
-    const[empleados, setEmpleados] = useState([]);
+    const [empleados, setEmpleados] = useState([]);
 
     useEffect(() => {
         cargarEmpleados();
@@ -19,35 +20,40 @@ export default function ListadoEmpleados() {
     }
 
     return (
-        <div className="container">
-            <div className="container text-center" style={{ margin: "30px" }}>
-                <h3>Sistema de Recursos Humanos</h3>
-            </div>
+        <IntlProvider locale="es">
+            <div className="container">
+                <div className="container text-center" style={{ margin: "30px" }}>
+                    <h3>Sistema de Recursos Humanos</h3>
+                </div>
 
-            <table className="table table-striped table-hover align-middle">
-                <thead className="table-dark">
-                    <tr>
-                        <th scope="col">Id</th>
-                        <th scope="col">Empleado</th>
-                        <th scope="col">Departamento</th>
-                        <th scope="col">Sueldo</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+                <table className="table table-striped table-hover align-middle">
+                    <thead className="table-dark">
+                        <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Empleado</th>
+                            <th scope="col">Departamento</th>
+                            <th scope="col">Sueldo</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {empleados.map((empleado, indice) => (
+                            <tr key={indice}>
+                                <th scope="row">{empleado.idEmpleado}</th>
+                                <td>{empleado.nombre}</td>
+                                <td>{empleado.departamento}</td>
+                                <td>
+                                    <td>${<FormattedNumber
+                                        value={empleado.sueldo}
+                                        style="decimal"
+                                        minimumFractionDigits={2}
+                                        maximumFractionDigits={2}
+                                    />}</td>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </IntlProvider>
     )
 }
