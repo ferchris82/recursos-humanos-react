3f6fbc41b490191f3407d062c96427c7b66a27c0
ChrisferDev <ferchris82@hotmail.com>
2024-03-18 15:13:08 -0500

    Versión 1.3

--- src/empleados/AgregarEmpleado.js
+++ src/empleados/AgregarEmpleado.js
@@ -1,26 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom'
 
 export default function AgregarEmpleado() {
+    let navegacion = useNavigate();
+
+    const [empleado, setEmpleado]=useState({
+        nombre:"",
+        departamento:"",
+        sueldo:""
+    })
+
+    const{nombre, departamento, sueldo} = empleado
+
+    const onInputChange = (e) => {
+        //spread operator ... (Expandir los atributos)
+        setEmpleado({...empleado, [e.target.name]: e.target.value})
+    }
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        const urlBase = "http://localhost:8080/rh-app/empleados";
+        await axios.post(urlBase, empleado);
+        //Redirigimos a la página de inicio.
+        navegacion('/');
+    }
+
     return (
         <div className='container'>
             <div className='container text-center' style={{ margin: "30px" }}>
                 <h3>Agregar Empleado</h3>
             </div>
-            <form>
+            <form onSubmit={(e) => onSubmit(e)}>
                 <div className="mb-3">
                     <label htmlFor="nombre" className="form-label">Nombre</label>
                     <input type="text" className="form-control"
-                     id="nombre" name='nombre' required={true}/>
+                     id="nombre" name='nombre' required={true}
+                     value={nombre} onChange={(e)=>onInputChange(e)}/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="departamento" className="form-label">Departamento</label>
                     <input type="text" className="form-control" 
-                    id="departamento" name='departamento'/>
+                    id="departamento" name='departamento'
+                    value={departamento} onChange={(e)=>onInputChange(e)}/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="sueldo" className="form-label">Sueldo</label>
                     <input type="number" step="any" className="form-control" 
-                    id="sueldo" name='sueldo'/>
+                    id="sueldo" name='sueldo'
+                    value={sueldo} onChange={(e)=>onInputChange(e)}/>
                 </div>
                 <div className='text-center'>
                 <button type="submit" className="btn btn-primary btn-sm me-3">Agregar</button>
