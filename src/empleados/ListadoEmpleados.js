1cc0d567113833db60d66950f77057403c39417e
ChrisferDev <ferchris82@hotmail.com>
2024-03-18 11:52:47 -0500

    Version 1.0

---
+++ src/empleados/ListadoEmpleados.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export default function ListadoEmpleados() {
+    return (
+        <div className="container">
+            <div className="container text-center" style={{ margin: "30px" }}>
+                <h3>Sistema de Recursos Humanos</h3>
+            </div>
+
+            <table className="table table-striped table-hover align-middle">
+                <thead className="table-dark">
+                    <tr>
+                        <th scope="col">Id</th>
+                        <th scope="col">Empleado</th>
+                        <th scope="col">Departamento</th>
+                        <th scope="col">Sueldo</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
